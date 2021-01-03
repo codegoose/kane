62e21232e4c174be2dfb06dce93ba546007807d7
Brandon White <b.white.1@pm.me>
2021-01-03 02:26:12 -0800

    Load images arbitrarily from assets folder.

--- assets.cpp
+++ assets.cpp
@@ -1,21 +1,35 @@
 #include "assets.h"
 #include "nanovg.h"
+#include "logging.h"
 
+#include <filesystem>
 #include <GL/glew.h>
 
 std::map<std::string, int> kane::assets::sprite_sheet_gl_handles;
 
 bool kane::assets::load(NVGcontext *nvg) {
-	int texture = nvgCreateImage(nvg, "C:\\Users\\Brandon\\Documents\\Assets\\merchant-idle.png", 0);
-	sprite_sheet_gl_handles["merchant-idle"] = texture;
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	for (auto &file : std::filesystem::directory_iterator("C:\\Users\\Brandon\\Documents\\Assets\\")) {
+		if (file.path().extension() != ".png") continue;
+		int texture = nvgCreateImage(nvg, file.path().string().c_str(), 0);
+		if (texture == 0) {
+			sl::warn("Unable to load file as image: {}", file.path().string());
+			continue;
+		}
+		auto name = file.path().stem().string();
+		sprite_sheet_gl_handles[name] = texture;
+		sl::debug("Loaded {} as GL texture #{}.", name, texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
 	return true;
 }
 
 void kane::assets::destroy(NVGcontext *nvg) {
-	for (auto &E : sprite_sheet_gl_handles) nvgDeleteImage(nvg, E.second);
+	for (auto &E : sprite_sheet_gl_handles) {
+		sl::debug("Destroyed GL texture #{}. ({})", E.second, E.first);
+		nvgDeleteImage(nvg, E.second);
+	}
 	sprite_sheet_gl_handles.clear();
 }
