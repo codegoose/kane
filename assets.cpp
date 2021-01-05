25c90d0680f913a3ca9629a20557607b7d614369
Brandon White <b.white.1@pm.me>
2021-01-05 00:32:35 -0800

    Expose sprite sheet pixel data.

--- assets.cpp
+++ assets.cpp
@@ -1,21 +1,44 @@
 #include "assets.h"
 #include "nanovg.h"
 #include "logging.h"
+#include "stb_image.h"
 
+#include <fstream>
 #include <filesystem>
 #include <GL/glew.h>
 
 std::map<std::string, int> kane::assets::sprite_sheet_gl_handles;
+std::map<std::string, std::pair<glm::ivec2, std::vector<uint8_t>>> kane::assets::sprite_sheet_pixel_data;
 
 bool kane::assets::load(NVGcontext *nvg) {
 	for (auto &file : std::filesystem::directory_iterator("C:\\Users\\Brandon\\Documents\\Assets\\")) {
 		if (file.path().extension() != ".png") continue;
-		int texture = nvgCreateImage(nvg, file.path().string().c_str(), 0);
-		if (texture == 0) {
-			sl::warn("Unable to load file as image: {}", file.path().string());
+		std::ifstream in(file.path().string().c_str(), std::ios::binary);
+		if (!in.is_open()) {
+			sl::warn("Unable to open image file: {}", file.path().string());
 			continue;
 		}
 		auto name = file.path().stem().string();
+		std::vector<char> contents((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+		int w, h, num_chan;
+		auto pixels = stbi_load_from_memory(reinterpret_cast<const stbi_uc *>(contents.data()), contents.size(), &w, &h, &num_chan, 4);
+		if (!pixels) {
+			sl::warn("Unable to process file data as image: {}", file.path().string());
+			continue;
+		}
+		int texture = 0;
+		{
+			auto &convenient_img_copy = sprite_sheet_pixel_data[name];
+			convenient_img_copy.first = { w, h };
+			convenient_img_copy.second.resize(w * h * 4);
+			memcpy(convenient_img_copy.second.data(), pixels, convenient_img_copy.second.size());
+			texture = nvgCreateImageRGBA(nvg, w, h, 0, pixels);
+			stbi_image_free(pixels);
+		}
+		if (!texture) {
+			sl::warn("Unable to create texture from image pixels: {}", file.path().string());
+			continue;
+		}
 		sprite_sheet_gl_handles[name] = texture;
 		sl::debug("Loaded {} as GL texture #{}.", name, texture);
 		glBindTexture(GL_TEXTURE_2D, texture);
--- assets.h
+++ assets.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <map>
+#include <vector>
+#include <cstdint>
 #include <string>
+#include <utility>
+
+#include <glm/vec2.hpp>
 
 #include "nanovg.h"
 
 namespace kane::assets {
 
 	extern std::map<std::string, int> sprite_sheet_gl_handles;
+	extern std::map<std::string, std::pair<glm::ivec2, std::vector<uint8_t>>> sprite_sheet_pixel_data;
 
 	bool load(NVGcontext *nvg);
 	void destroy(NVGcontext *nvg);
