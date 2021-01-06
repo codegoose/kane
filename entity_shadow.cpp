3517c1d45020205cabe3ebec2aa6dfba6c792e46
Brandon White <b.white.1@pm.me>
2021-01-05 17:41:35 -0800

    Add shadow entity.

---
+++ entity_shadow.cpp
@@ -0,0 +1,91 @@
+#include "entity_shadow.h"
+#include "logging.h"
+
+kane::entity::shadow_entity::shadow_entity() {
+	current_anim = "shadow-idle";
+	anims["shadow-idle"];
+	anims["shadow-run"];
+	anims["shadow-charge"];
+	anims["shadow-damage"];
+	anims["shadow-attack-1"];
+	anims["shadow-attack-2"];
+}
+
+void kane::entity::shadow_entity::update(double secs) {
+	state += secs;
+	sl::info("{}", state);
+	if (state < 3.f) {
+		current_anim = "shadow-run";
+		pos.x += (flipped ? -10 : 10) * secs;
+		if (pos.x > 50) flipped = true;
+		if (pos.x < -50) flipped = false;
+	} else if (state < 6.f) {
+		current_anim = "shadow-charge";
+	} else if (state < 12.f) {
+		current_anim = "shadow-idle";
+	} else {
+		state = 0.f;
+	}
+}
+
+void kane::entity::shadow_entity::anim_sheet_assign_cb(std::string anim_name, int sprite_sheet_gl_handle, glm::ivec2 sheet_size) {
+	auto &anim = anims[anim_name];
+	if (anim_name == "shadow-idle") {
+		anim.sheet_img = sprite_sheet_gl_handle;
+		anim.sheet_size = sheet_size;
+		anim.tile_size = {
+			anim.sheet_size.x,
+			anim.sheet_size.y
+		};
+		anim.tile_off = {
+			-anim.tile_size.x / 2,
+			-anim.tile_size.y
+		};
+		anim.num_wait_steps = 1;
+		anim.frame_xy_list = {
+			{ 0, 0 }
+		};
+	} else if (anim_name == "shadow-run") {
+		anim.sheet_img = sprite_sheet_gl_handle;
+		anim.sheet_size = sheet_size;
+		anim.tile_size = {
+			anim.sheet_size.x,
+			anim.sheet_size.y / 6
+		};
+		anim.tile_off = {
+			-anim.tile_size.x / 2,
+			-anim.tile_size.y
+		};
+		anim.num_wait_steps = 4;
+		anim.frame_xy_list = {
+			{ 0, 0 },
+			{ 0, 1 },
+			{ 0, 2 },
+			{ 0, 3 },
+			{ 0, 4 },
+			{ 0, 5 }
+		};
+	} else if (anim_name == "shadow-charge") {
+		anim.sheet_img = sprite_sheet_gl_handle;
+		anim.sheet_size = sheet_size;
+		anim.tile_size = {
+			anim.sheet_size.x,
+			anim.sheet_size.y / 8
+		};
+		anim.tile_off = {
+			-anim.tile_size.x / 2,
+			-anim.tile_size.y
+		};
+		anim.num_wait_steps = 2;
+		anim.frame_xy_list = {
+			{ 0, 0 },
+			{ 0, 1 },
+			{ 0, 2 },
+			{ 0, 3 },
+			{ 0, 4 },
+			{ 0, 5 },
+			{ 0, 6 },
+			{ 0, 7 }
+		};
+	}
+}
