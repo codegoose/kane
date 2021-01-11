ae718c034de25bc48e764a70d3ff0738eddd94d2
Brandon White <b.white.1@pm.me>
2021-01-11 08:01:22 -0800

    Implement mud minion.

---
+++ source/kane/entity_mud_minion.cpp
@@ -0,0 +1,86 @@
+#include <kane/entity_mud_minion.h>
+
+kane::pc::mud_minion_entity::mud_minion_entity() {
+	current_anim = "mud_minion_damage";
+	anims["mud_minion_move"];
+	anims["mud_minion_attack"];
+	anims["mud_minion_damage"];
+}
+
+kane::pc::mud_minion_entity::~mud_minion_entity() {
+	
+}
+
+void kane::pc::mud_minion_entity::anim_sheet_assign_cb(std::string anim_name, int sprite_sheet_gl_handle, glm::ivec2 sheet_size) {
+	auto &anim = anims[anim_name];
+	if (anim_name == "mud_minion_move") {
+		anim.sheet_img = sprite_sheet_gl_handle;
+		anim.sheet_size = sheet_size;
+		anim.tile_size = {
+			anim.sheet_size.x,
+			anim.sheet_size.y / 4
+		};
+		anim.tile_off = {
+			-anim.tile_size.x / 2,
+			-anim.tile_size.y
+		};
+		anim.num_wait_steps = 3;
+		anim.frame_xy_list = {
+			{ 0, 0 },
+			{ 0, 1 },
+			{ 0, 2 },
+			{ 0, 3 }
+		};
+	} else if (anim_name == "mud_minion_attack") {
+		anim.sheet_img = sprite_sheet_gl_handle;
+		anim.sheet_size = sheet_size;
+		anim.tile_size = {
+			anim.sheet_size.x,
+			anim.sheet_size.y / 8
+		};
+		anim.tile_off = {
+			-anim.tile_size.x / 2,
+			-anim.tile_size.y
+		};
+		anim.num_wait_steps = 3;
+		anim.frame_xy_list = {
+			{ 0, 0 },
+			{ 0, 1 },
+			{ 0, 2 },
+			{ 0, 3 },
+			{ 0, 4 },
+			{ 0, 5 },
+			{ 0, 6 },
+			{ 0, 7 }
+		};
+		anim.loop = false;
+		anim.next_anim = "mud_minion_move";
+	} else if (anim_name == "mud_minion_damage") {
+		anim.sheet_img = sprite_sheet_gl_handle;
+		anim.sheet_size = sheet_size;
+		anim.tile_size = {
+			anim.sheet_size.x,
+			anim.sheet_size.y / 8
+		};
+		anim.tile_off = {
+			-anim.tile_size.x / 2,
+			-anim.tile_size.y
+		};
+		anim.num_wait_steps = 3;
+		anim.frame_xy_list = {
+			{ 0, 0 },
+			{ 0, 1 },
+			{ 0, 2 },
+			{ 0, 3 },
+			{ 0, 4 },
+			{ 0, 5 },
+			{ 0, 6 },
+			{ 0, 7 }
+		};
+		anim.loop = false;
+	}
+}
+
+void kane::pc::mud_minion_entity::anim_frame_cb(int frame) {
+
+}
